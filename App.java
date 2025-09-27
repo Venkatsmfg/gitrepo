1ab745ad30ba6929b5a045a7e236cec801c6f714
Venkatsmfg <durgaraoyanamadala25@gmail.com>
2025-09-27 23:59:00 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven World!");
+    }
+
+    // Simple method to test
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
